48621e1b5d7607f33c28760a71725d9023a4f843
hamdirealforce <hamdi@realforce.ch>
2022-09-27 15:26:29 +0100

    second update

--- Node_App1/server.js
+++ Node_App1/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+var client = require("./connection.js");
 
 
 const bodyParser = require('body-parser');
@@ -17,10 +18,21 @@ const HOST = '0.0.0.0';
 // Imports the Google Cloud client library
 const {PubSub} = require('@google-cloud/pubsub');
 
+
+
+
+
+
 // Creates a client; cache this for further use
 const pubSubClient = new PubSub();
 // App
+client.cluster.health({},function(err,resp,status) {  
+    console.log("-- Client Health --",resp);
+  })
+  
 
+  
+  
 async function publishMessage() {
   // Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
   const dataBuffer = Buffer.from(data);
@@ -30,6 +42,20 @@ async function publishMessage() {
       .topic(topicNameOrId)
       .publishMessage({data: dataBuffer});
     console.log(`Message ${messageId} published.`);
+    
+    
+    
+    client.indices.create({  
+  index: messageId
+},function(err,resp,status) {
+  if(err) {
+    console.log(err);
+  }
+  else {
+    console.log("Create index response: ",resp);
+  }
+});
+  
   } catch (error) {
     console.error(`Received error while publishing: ${error.message}`);
     process.exitCode = 1;
